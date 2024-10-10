961bd66cef84c7f62b17abdc7c77a5869f50948f
Sandhosh <sandhoshtechie02@gmail.com>
2024-10-10 12:07:20 +0530

    cahpter 4 end

--- AI Resume Builder/AI_RESUME_BUILDER/src/App.jsx
+++ AI Resume Builder/AI_RESUME_BUILDER/src/App.jsx
@@ -6,7 +6,7 @@ function App  () {
   const [count ,setCount]= useState(0)
   const{user,isLoaded,isSignedIn}=useUser();
 
-  if (!isSignedIn)
+  if (!isSignedIn&&isLoaded)
   {
     return <Navigate to ={'/auth/sign-in'}/>
   }
--- AI Resume Builder/AI_RESUME_BUILDER/src/auth/sign-in/index.jsx
+++ AI Resume Builder/AI_RESUME_BUILDER/src/auth/sign-in/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { SignIn } from '@clerk/clerk-react';
+import { SignIn, UserButton } from '@clerk/clerk-react';
 
 function SignInpage() {
   return (
     <div className='flex justify-center my-20 items-center'>
       <SignIn/>
+      
     </div>
   );
 }
